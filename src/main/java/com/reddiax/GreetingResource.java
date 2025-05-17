359e7b83f0be2e146f51bb5e5e46197b246df572
Konrad <konrad.stobiecki@gmail.com>
2025-05-17 16:39:55 +0200

    initial

---
+++ src/main/java/com/reddiax/GreetingResource.java
@@ -0,0 +1,23 @@
+package com.reddiax;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+
+@Path("/hello")
+public class GreetingResource {
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        return "Hello from Quarkus REST";
+    }
+
+    @GET
+    @Path("/world")
+    @Produces(MediaType.TEXT_PLAIN)
+    public String world() {
+        return "world from Quarkus REST";
+    }
+}
